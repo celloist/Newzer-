a5af4f876b03a1ba1dc80e3fced6de2b76bdeaa5
celloist <alhriclacle@gmail.com>
2024-03-26 21:07:40 +0100

    try again

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,14 @@
 terraform {
-    backend "s3" {    
-          region = "eu-central-1"   
-          key    = "terraform.tfstate"  
-        } 
+    required_providers {    
+      aws = {      
+        source = "hashicorp/aws"    
+      }  
+    }  
+  backend "s3" {
+    region = "eu-central-1"   
+    key    = "terraform.tfstate"
+  }
 }
-
 
 provider "aws" {
  region = "eu-central-1"
