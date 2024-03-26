3ff61db7115395bcefd99561107d03e02e35b355
celloist <alhriclacle@gmail.com>
2024-03-26 19:48:02 +0100

    add terraform provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,8 +5,30 @@ terraform {
       version = "~> 5.41.0"
     }
   }
+   backend "s3" {
+
+   region = "eu-central-1"
+
+   key    = "terraform.tfstate"
+
+ }
+
 }
  
 provider "aws" {
   region = "eu-central-1"
+}
+
+resource "aws_instance" "tf_instance" {
+
+ ami           = "ami-830c94e3"
+
+ instance_type = "t2.nano"
+
+ tags = {
+
+   Name = "tf_instance"
+
+ }
+
 }
