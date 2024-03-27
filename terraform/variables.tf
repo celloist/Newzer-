560d43f40253a0cbbf1d6298a6ccdd6c2d27653c
celloist <alhriclacle@gmail.com>
2024-03-27 09:12:23 +0100

    incorrect naming

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,12 +5,12 @@ variable "api_domain" {
 
 variable "name_prefix" {
   description = "Name prefix to use for objects that need to be created (only lowercase alphanumeric characters and hyphens allowed, for S3 bucket name compatibility)"
-  default     = "athmare-aws-lambda-api---"
+  default     = "lambda-api"
 }
 
 variable "comment_prefix" {
   description = "This will be included in comments for resources that are created"
-  default     = "Lambda API: "
+  default     = "lamda api"
 }
 
 variable "function_zipfile" {
